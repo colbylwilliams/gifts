8a37bb38cc1305201b36b2c6196e348266303b68
Colby Williams <colbylwilliams@gmail.com>
2017-11-19 00:39:40 -0500

    gitignore + workspace + ios project + carthage + azuredata

---
+++ gifts/gifts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gifts
+//
+//  Created by Colby L Williams on 11/19/17.
+//  Copyright © 2017 Colby L Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
